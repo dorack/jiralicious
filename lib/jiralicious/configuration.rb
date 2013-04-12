e95e9f85e8712bafdac8b3722b91d32e18b89a82
Stanley Handschuh <stanleyhandschuh@yahoo.com>
2013-04-12 08:32:00 -0500

    YAML Configuration
    I have extended the configuration to allow for loading of YAML. It is
    currently using HASH/String syntax to retrieve the information from the
    YAML file. However it may be able to do it via symbols as well (never
    tested it).

--- lib/jiralicious/configuration.rb
+++ lib/jiralicious/configuration.rb
@@ -1,11 +1,12 @@
 # encoding: utf-8
+require 'ostruct'
 
 module Jiralicious
   module Configuration
     VALID_OPTIONS = [:username, :password, :uri, :api_version, :auth_type]
     DEFAULT_USERNAME = nil
-    DEFAULT_AUTH_TYPE = :basic
     DEFAULT_PASSWORD = nil
+    DEFAULT_AUTH_TYPE = :basic
     DEFAULT_URI = nil
     DEFAULT_API_VERSION = "latest"
 
@@ -33,5 +34,19 @@ module Jiralicious
       self.api_version = DEFAULT_API_VERSION
       self.auth_type = DEFAULT_AUTH_TYPE
     end
+
+	def load_yml(yml_file)
+		if File.exist?(yml_file)
+			yml_os = OpenStruct.new(YAML.load_file(yml_file))
+			self.configure do |config|
+				config.username = yml_os.jira['username']
+				config.password = yml_os.jira['password']
+				config.uri = yml_os.jira['host']
+				config.auth_type = yml_os.jira['auth_type']
+			end
+		else
+			reset
+		end
+	end
   end
 end
