dbb24b7f35e17e99e0884bc5de19225c27fddf63
Stephen Eckenrode <seckenrode@panoramaed.com>
2016-03-30 08:42:57 -0400

    Allow numbers in project keys

--- lib/jiralicious/base.rb
+++ lib/jiralicious/base.rb
@@ -144,7 +144,7 @@ module Jiralicious
       #
       #
       def issueKey_test(key, no_throw = false)
-        if key.nil? || !(/^[A-Z]+-[0-9]+$/i =~ key)
+        if key.nil? || !(/^[A-Z][A-Z0-9_]*-[0-9]+$/i =~ key)
           raise Jiralicious::JiraError.new("The key #{key} is invalid") unless no_throw
           return false
         end
