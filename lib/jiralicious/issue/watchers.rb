096d9020644ca8ec5572c837785922074d16921d
Grant Ridder <shortdudey123@gmail.com>
2016-12-07 15:03:55 -0800

    Fix Style/NilComparison offenses

--- lib/jiralicious/issue/watchers.rb
+++ lib/jiralicious/issue/watchers.rb
@@ -15,7 +15,7 @@ module Jiralicious
       # Initialization Method
       #
       def initialize(decoded_json = nil)
-        if (decoded_json != nil)
+        unless (decoded_json.nil?)
           properties_from_hash(decoded_json)
           super(decoded_json)
           parse!(decoded_json)
